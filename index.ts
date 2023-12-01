cbfd2b0c2edc1acb3878b8bc9e5300348b525770
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-12-01 12:11:59 +0100

    refactor: edit package

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import createClient from 'openapi-fetch'
+import createOpenAPIClient from 'openapi-fetch'
 import { paths as v2_paths } from './third_party/v2'
 
-export type ClientV2 = ReturnType<typeof createClient<v2_paths>>
+export type Client = ReturnType<typeof createOpenAPIClient<v2_paths>>
 
-export function createClientV2({ token }: { token: string }) {
-  return createClient<v2_paths>({
+export function createClient({ token }: { token: string }) {
+  return createOpenAPIClient<v2_paths>({
     baseUrl: 'https://api.digitalocean.com',
     headers: {
       Authorization: `Bearer ${token}`
